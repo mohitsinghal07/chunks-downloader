14557676723937653f57627eb5d8b0601e5ba031
Mohit Singhal <mohitsinghal321@gmail.com>
2023-03-23 10:38:15 +0530

    download, concatenate and convert to mp4 and delete all the chunks

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const https = require('https');
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+const { spawn } = require('child_process');
 
 // Replace the API endpoint with your own
 // https://hsprepack.akamaized.net/videos/bharat/rakrsb/2/master_Layer4_00012.ts
@@ -22,6 +23,7 @@ if (!fs.existsSync('downloads')) {
 
 // Send the GET request to the API endpoint multiple times
 const numOfRequests = 5;
+const chunkFilenames = [];
 for (let i = 1; i <= numOfRequests; i++) {
     const fileNumber = i.toString().padStart(5, '0'); // convert the file number to string and pad with leading zeros
   
@@ -32,15 +34,37 @@ for (let i = 1; i <= numOfRequests; i++) {
   
     // Send the GET request to the API endpoint
     https.get(newUrl, options, res => {
-      // Create a write stream to save the file
-      const fileStream = fs.createWriteStream(filename);
-  
-      // Pipe the response stream to the write stream to save the file
-      res.pipe(fileStream);
-  
-      // Handle errors during the download
-      fileStream.on('error', err => console.error(err));
-      res.on('error', err => console.error(err));
-    });
+        // Create a write stream to save the file
+        const fileStream = fs.createWriteStream(filename);
+    
+        // Pipe the response stream to the write stream to save the file
+        res.pipe(fileStream);
+    
+        // Handle errors during the download
+        fileStream.on('error', err => console.error(err));
+        res.on('error', err => console.error(err));
+        res.on('end', () => {
+          console.log(`Downloaded ${filename}`);
+          chunkFilenames.push(filename);
+          if (chunkFilenames.length === numOfRequests) {
+            // Sort the chunk filenames in ascending order
+            chunkFilenames.sort();
+
+            // Concatenate the chunks into an MP4 file
+            const mp4Filename = 'downloads/output.mp4';
+            const ffmpeg = spawn('ffmpeg', ['-i', `concat:${chunkFilenames.join('|')}`, '-c', 'copy', mp4Filename]);
+    
+            // Handle errors during the concatenation
+            ffmpeg.stderr.on('data', data => console.error(data.toString()));
+            ffmpeg.on('exit', () => {
+              console.log(`Finished concatenating the chunks into ${mp4Filename}`);
+              // Delete the chunk files
+              for (const filename of chunkFilenames) {
+                fs.unlinkSync(filename);
+              }
+            });
+          }
+        });
+      });
   }
 
